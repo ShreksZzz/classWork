33831c14578d8c03739fddd7666ad34133ac0d29
ShreksZzz <alexmatveev24@mail.ru>
2024-02-07 10:58:42 +0300

    Функции 3

--- task3/app.js
+++ task3/app.js
@@ -1,45 +1,11 @@
-// const person = {
-//     name: "Stepan",
-//     lastName: "Mihalich",
-//     age: 32,
-//     hobby: ["MovieMaking"],
-//     adress: {
-//         home: 'ул. Пушкина, дом Колотушкина',
-//         city: 'г. Вогоград'
-//     },
-//     sayHello: function() {
-//         console.log('Два по корпусу!!!');
-//     }
-// };
-
-// console.log(person)
-
-const collages = [
-//студент
-{
-name: "Григорий",
-surname: "Лепс",
-age: 17,
-friends: ["Олег", "Петя", "Артем"],
-parasToday: {
-    first: "Матан",
-    second: "МДК",
-    third: "БЖ",
-    }
-},
-//Препод
-{
-name: "Михал",
-surname: "Палыч",
-age: 30,
-subjects: ["Матан", "Теория вероятности", "Дискретная математика"],
-groupsToday: {
-    first: "ПБшники",
-    second: "Программисты",
-    third: "СисАдмины"
+function getMax (a, b) {
+    if (a > b) {
+        return a;
+    } else if (b > a) {
+        return b;
+    } else {
+        return(a);
     }
 }
-];
 
-console.log(collages[0] ["name"], collages[0] ["surname"], collages[0] ["parasToday"]);
-console.log(collages[1] ["name"], collages[1] ["surname"], collages[1] ["subjects"]);
+console.log(getMax(1, 3));
