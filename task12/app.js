04d26d66a2af65ba03ceaa3d133b6cfd6139bd4f
IN1kky <Sigitov.349@yandex.ru>
2024-02-09 13:35:02 +0300

    Кредит

---
+++ task12/app.js
@@ -0,0 +1,16 @@
+// const title = document.querySelector(".title");
+// let res = title.innerText;
+
+// if(res === "Лето") {
+//     alert("Надень футболку с трусами");
+// } else if (res === "Весна"){
+//     alert("Надень кофту с трусами");
+// } else if (res === "Зима"){
+//     alert("Натяни на голову гондон");
+// } else if ("Осень") {
+//     alert("Ты сдохнуть хочешь тварь блять?")
+// }
+
+
+let credit = document.querySelector(".credit");
+let years = document.querySelector(".years");
